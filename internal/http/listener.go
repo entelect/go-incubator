27f6468cc8e6546fee73874d34bcd200422f2d11
ENTELECT\dewald.swanepoel <dewald.swanepoel@entelect.co.za>
2022-10-12 18:18:07 +0200

    Adding HTTP handlers

--- internal/http/listener.go
+++ internal/http/listener.go
@@ -2,8 +2,13 @@ package http
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"sort"
+	"strings"
 	"sync"
 	"time"
 )
@@ -22,6 +27,7 @@ func NewHttpServer(port int) (HttpServer, error) {
 	}
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(s.router))
 	s.server.Handler = mux
 
 	return s, nil
@@ -49,4 +55,132 @@ func (s *HttpServer) Stop() {
 	if err := s.server.Shutdown(ctxTimeout); err != nil {
 		panic(err) // failure/timeout shutting down the server gracefully
 	}
+}
+
+// router is the top level Handler which directs incoming traffic to the appropriate endpoint Handlers
+func (s *HttpServer) router(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" && r.RequestURI == "/recipe" {
+		s.addRecipe(w, r)
+		return
+	}
+
+	if r.Method == "GET" && strings.HasPrefix(r.RequestURI, "/recipe/") {
+		s.getRecipe(w, r)
+		return
+	}
+
+	if r.Method == "GET" && strings.HasPrefix(r.RequestURI, "/recipes") {
+		s.findRecipes(w, r)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+}
+
+// addRecipe is the Handler for adding new recipes
+func (s *HttpServer) addRecipe(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+
+	recipe := Recipe{}
+	err := json.Unmarshal(body, &recipe)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("error unmarshalling recipe"))
+		return
+	}
+
+	if recipe.Name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("no name specified"))
+		return
+	}
+
+	if len(recipe.Ingredients) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("no ingredients specified"))
+		return
+	}
+
+	s.recipes[recipe.Name] = recipe
+}
+
+// getRecipe is the Handler for retrieving a recipe by name
+func (s *HttpServer) getRecipe(w http.ResponseWriter, r *http.Request) {
+	name, err := url.QueryUnescape(strings.TrimPrefix(r.RequestURI, "/recipe/"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	recipe, ok := s.recipes[name]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	rsp, err := json.Marshal(recipe)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error marshalling recipe into json"))
+	}
+
+	w.Write(rsp)
+}
+
+// findRecipes is the Handler for listing recipes by ingredients
+func (s *HttpServer) findRecipes(w http.ResponseWriter, r *http.Request) {
+	unescaped, err := url.QueryUnescape(strings.TrimPrefix(r.RequestURI, "/recipes"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	elems := strings.Split(unescaped, "?")
+	if len(elems) > 2 {
+		w.WriteHeader((http.StatusBadRequest))
+	}
+
+	var params []string
+	if len(elems) > 1 {
+		params = strings.Split(elems[1], "&")
+	}
+
+	var ingredients []string
+	for _, v := range params {
+		if strings.HasPrefix(v, "ingredients=") {
+			ingredients = strings.Split(strings.TrimPrefix(v, "ingredients="), ",")
+		}
+	}
+
+	if len(ingredients) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("no ingredients specified"))
+		return
+	}
+
+	// We want to return the list of recipes in alphabetical order (by name)
+	// To do that, we first extract map keys into a slice, then sort the slice,
+	// then iterate over the slice, to obtain map entries in alphabetical order
+	keys := make([]string, 0, len(s.recipes))
+	for k := range s.recipes {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	recipes := make([]Recipe, 0)
+	for _, k := range keys {
+		recipe := s.recipes[k]
+		if recipe.UsesIngredients(ingredients) {
+			recipes = append(recipes, recipe)
+		}
+	}
+
+	rsp, err := json.Marshal(recipes)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("error marshalling recipes into json"))
+	}
+
+	w.Write(rsp)
 }
--- internal/http/listener_test.go
+++ internal/http/listener_test.go
@@ -3,7 +3,9 @@ package http
 import (
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -47,6 +49,201 @@ func TestNewHttpServer(t *testing.T) {
 			}
 			if got.server.Addr != tt.want.server.Addr || !reflect.DeepEqual(got.recipes, tt.want.recipes) {
 				t.Errorf("NewHttpServer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHttpServer_addRecipe(t *testing.T) {
+	server, _ := NewHttpServer(1234)
+
+	type response struct {
+		code int
+		body string
+	}
+
+	tests := []struct {
+		name string
+		body string
+		want response
+	}{
+		{
+			name: "1",
+			body: `{"name":"Meatballs","ingredients":["Ground Beef","Tomato"]}`,
+			want: response{
+				code: http.StatusOK,
+				body: ``,
+			},
+		},
+		{
+			name: "2",
+			body: `{"ingredients":["Mozzarella","Macaroni"]}`,
+			want: response{
+				code: http.StatusBadRequest,
+				body: `no name specified`,
+			},
+		},
+		{
+			name: "3",
+			body: `{"name":"Pizza"}`,
+			want: response{
+				code: http.StatusBadRequest,
+				body: `no ingredients specified`,
+			},
+		},
+		{
+			name: "4",
+			body: `{"name":"Meatballs","ingredients":["Ground Beef,"Tomato"]}`,
+			want: response{
+				code: http.StatusBadRequest,
+				body: `error unmarshalling recipe`,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", "/recipe", strings.NewReader(tt.body))
+			server.addRecipe(w, r)
+
+			if w.Code != tt.want.code || w.Body.String() != tt.want.body {
+				t.Errorf("addRecipe() = %v, want %v", response{code: w.Code, body: w.Body.String()}, tt.want)
+			}
+		})
+	}
+}
+
+func TestHttpServer_getRecipe(t *testing.T) {
+	server, _ := NewHttpServer(1234)
+	server.recipes["Cheese Fondue"] = Recipe{Name: "Cheese Fondue", Ingredients: []string{"Gruyere", "Emmental"}}
+	server.recipes["Mac & Cheese"] = Recipe{Name: "Mac & Cheese", Ingredients: []string{"Mozzarella", "Macaroni"}}
+	server.recipes["SpagBol"] = Recipe{Name: "SpagBol", Ingredients: []string{"Spaghetti", "Ground Beef", "Tomato"}}
+	server.recipes["BLT"] = Recipe{Name: "BLT", Ingredients: []string{"Tomato", "Bacon", "Lettuce"}}
+	server.recipes["Greek Salad"] = Recipe{Name: "Greek Salad", Ingredients: []string{"Feta", "Tomato", "Cucumber"}}
+	server.recipes["Caprese Salad"] = Recipe{Name: "Caprese Salad", Ingredients: []string{"Mozzarella", "Tomato"}}
+	server.recipes["Meatballs"] = Recipe{Name: "Meatballs", Ingredients: []string{"Ground Beef", "Tomato"}}
+
+	type response struct {
+		code int
+		body string
+	}
+
+	tests := []struct {
+		name string
+		path string
+		body string
+		want response
+	}{
+		{
+			name: "1",
+			path: "/recipe/Cheese%20Fondue",
+			want: response{
+				code: http.StatusOK,
+				body: `{"name":"Cheese Fondue","ingredients":["Gruyere","Emmental"]}`,
+			},
+		},
+		{
+			name: "2",
+			path: "/recipe/SpagBol",
+			want: response{
+				code: http.StatusOK,
+				body: `{"name":"SpagBol","ingredients":["Spaghetti","Ground Beef","Tomato"]}`,
+			},
+		},
+		{
+			name: "3",
+			path: "/recipe/Pizza",
+			want: response{
+				code: http.StatusNotFound,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", tt.path, strings.NewReader(tt.body))
+			server.getRecipe(w, r)
+
+			if w.Code != tt.want.code || w.Body.String() != tt.want.body {
+				t.Errorf("getRecipe() = %v, want %v", response{code: w.Code, body: w.Body.String()}, tt.want)
+			}
+		})
+	}
+}
+
+func TestHttpServer_findRecipes(t *testing.T) {
+	server, _ := NewHttpServer(1234)
+	server.recipes["Cheese Fondue"] = Recipe{Name: "Cheese Fondue", Ingredients: []string{"Gruyere", "Emmental"}}
+	server.recipes["Mac & Cheese"] = Recipe{Name: "Mac & Cheese", Ingredients: []string{"Mozzarella", "Macaroni"}}
+	server.recipes["SpagBol"] = Recipe{Name: "SpagBol", Ingredients: []string{"Spaghetti", "Ground Beef", "Tomato"}}
+	server.recipes["BLT"] = Recipe{Name: "BLT", Ingredients: []string{"Tomato", "Bacon", "Lettuce"}}
+	server.recipes["Greek Salad"] = Recipe{Name: "Greek Salad", Ingredients: []string{"Feta", "Tomato", "Cucumber"}}
+	server.recipes["Caprese Salad"] = Recipe{Name: "Caprese Salad", Ingredients: []string{"Mozzarella", "Tomato"}}
+	server.recipes["Meatballs"] = Recipe{Name: "Meatballs", Ingredients: []string{"Ground Beef", "Tomato"}}
+
+	type response struct {
+		code int
+		body string
+	}
+
+	tests := []struct {
+		name string
+		path string
+		body string
+		want response
+	}{
+		{
+			name: "1",
+			path: "/recipes?ingredients=Gruyere,Emmental",
+			want: response{
+				code: http.StatusOK,
+				body: `[{"name":"Cheese Fondue","ingredients":["Gruyere","Emmental"]}]`,
+			},
+		},
+		{
+			name: "2",
+			path: "/recipes?ingredients=Emmental,Gruyere",
+			want: response{
+				code: http.StatusOK,
+				body: `[{"name":"Cheese Fondue","ingredients":["Gruyere","Emmental"]}]`,
+			},
+		},
+		{
+			name: "3",
+			path: "/recipes?ingredients=Tomato",
+			want: response{
+				code: http.StatusOK,
+				body: `[{"name":"BLT","ingredients":["Tomato","Bacon","Lettuce"]},{"name":"Caprese Salad","ingredients":["Mozzarella","Tomato"]},{"name":"Greek Salad","ingredients":["Feta","Tomato","Cucumber"]},{"name":"Meatballs","ingredients":["Ground Beef","Tomato"]},{"name":"SpagBol","ingredients":["Spaghetti","Ground Beef","Tomato"]}]`,
+			},
+		},
+		{
+			name: "4",
+			path: "/recipes?ingredients=Tomato,Onion",
+			want: response{
+				code: http.StatusOK,
+				body: `[]`,
+			},
+		},
+		{
+			name: "5",
+			path: "/recipes",
+			want: response{
+				code: http.StatusBadRequest,
+				body: `no ingredients specified`,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", tt.path, strings.NewReader(tt.body))
+			server.findRecipes(w, r)
+
+			if w.Code != tt.want.code || w.Body.String() != tt.want.body {
+				t.Errorf("findRecipes() = %v, want %v", response{code: w.Code, body: w.Body.String()}, tt.want)
 			}
 		})
 	}
