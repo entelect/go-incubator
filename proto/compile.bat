d1bc5f91cd42172f8322f5ac60c1324f242f7698
ENTELECT\dewald.swanepoel <dewald.swanepoel@entelect.co.za>
2022-10-20 08:37:53 +0200

    Update .proto file and compiler

--- proto/compile.bat
+++ proto/compile.bat
@@ -1,1 +1,2 @@
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=require_unimplemented_servers=false,paths=source_relative recipesvc.proto
+protoc -I. -I"C:\Development\googleapis" --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=require_unimplemented_servers=false,paths=source_relative recipesvc.proto
+protoc -I. -I"C:\Development\googleapis" --grpc-gateway_out=. --grpc-gateway_opt=logtostderr=true --grpc-gateway_opt=paths=source_relative recipesvc.proto
